96bc679e5d0aabdb1aff985aeee90c00f29f8289
Nessun Kim <nessunkim@gmail.com>
2021-07-25 23:52:04 +0900

    Fix migration

--- migrations/2021-07-25-140540_add_some_boards/up.sql
+++ migrations/2021-07-25-140540_add_some_boards/up.sql
@@ -1,3 +1,4 @@
 INSERT INTO boards (display_name, name) VALUES ('자유게시판', 'freeboard');
 INSERT INTO boards (display_name, name) VALUES ('익명게시판', 'anon');
 INSERT INTO boards (display_name, name) VALUES ('조합게시판', 'coop');
+INSERT INTO boards (display_name, name) VALUES ('이슈트래커', 'dev_issue');
